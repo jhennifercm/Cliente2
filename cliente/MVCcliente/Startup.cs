a2047b32eb6f19bb906cb6a169258c075e2bd0f9
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-04-05 11:07:33 -0400

    commit mvc cliente

---
+++ cliente/MVCcliente/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCcliente.Startup))]
+namespace MVCcliente
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
