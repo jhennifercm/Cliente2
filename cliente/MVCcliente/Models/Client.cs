cc77c89c157e015fba06a330faf59fdf6c07735f
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-04-05 21:27:46 -0400

    commit mvc completo

---
+++ cliente/MVCcliente/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCcliente.Models
+{
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Correo { get; set; }
+
+    }
+}
---
+++ cliente/MVCcliente/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCcliente.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCcliente.Models.Client> Clients { get; set; }
+    }
+}
